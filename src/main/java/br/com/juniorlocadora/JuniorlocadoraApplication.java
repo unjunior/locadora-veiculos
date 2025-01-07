ac74a6a6cfca12914a41e2572d899e3d19fd6678
Ubirajara Junior <unjunior4315@gmail.com>
2025-01-06 21:07:08 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/juniorlocadora/JuniorlocadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.juniorlocadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorlocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorlocadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/juniorlocadora/JuniorlocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juniorlocadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorlocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
